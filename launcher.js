0c5d0343f37e5428a76124148aa82ec1d2381111
sumedhap12 <76724959+sumedhap12@users.noreply.github.com>
2021-06-23 09:29:51 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,29 @@
+class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 1
+        }
+        this.sling = Constraint.create(options);
+        this.pointB=pointB;
+        World.add(world, this.sling);
+
+    }
+
+    fly(){
+        this.sling.bodyA =null;
+        
+    }
+    display(){
+        if(this.sling.bodyA){
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+        
+        strokeWeight(3);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+        }
+    
+}
